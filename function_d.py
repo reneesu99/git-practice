194ec0af49d6fe6654633cefeddfeb472b924c51
Paje <ejap99@gmail.com>
2022-09-23 16:00:28 -0500

    Add function d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,8 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    sorted_numbers = numbers.sort(reverse = True)
+    return sorted_numbers[0]
 
 
 if __name__ == "__main__":
